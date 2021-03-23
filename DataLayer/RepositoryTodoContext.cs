e8e0c96922e675ff38bb4ef2b804bd9890ed5eee
Alex Semynin <alex.semynin1997@outlook.com>
2021-03-23 20:05:58 +0300

    Добавить контекст (EF) для работы с данными

---
+++ DataLayer/RepositoryTodoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class RepositoryTodoContext : DbContext
+    {
+        public RepositoryTodoContext(DbContextOptions<RepositoryTodoContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
