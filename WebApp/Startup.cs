7f5fbdf6635f79a4be27760afa712ae3df8c880f
Alex Semynin <alex.semynin1997@outlook.com>
2021-03-23 21:58:01 +0300

    настройка конфигурации проекта

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,21 +1,58 @@
+using BusinessLayer;
+using BusinessLayer.Implements;
+using BusinessLayer.Interfaces;
+using DataLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace WebApp
 {
     public class Startup
     {
+        private readonly string _clientAppLocation;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            string appLoacation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                                  ?? throw new ArgumentNullException(nameof(appLoacation));
+
+            _clientAppLocation = Path.Combine(appLoacation, "../WebClient/wwwroot");
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllersWithViews();
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = _clientAppLocation; });
+
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<RepositoryTodoContext>(options =>
+                options.UseSqlServer(connection)
+            );
+
+            services.AddTransient<IUsersRepo, EFUsersRepository>();
+            services.AddTransient<ITodosRepo, EFTodosRepository>();
+
+            services.AddScoped<DataManager>();
+
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +62,24 @@ namespace WebApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller}/{action=Index}/{id?}");
             });
+
+            app.UseSpa(spa => { spa.Options.SourcePath = _clientAppLocation; });
         }
     }
 }
