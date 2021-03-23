5d853b56e40923561678faebc0f7c7d29a088936
Alex Semynin <alex.semynin1997@outlook.com>
2021-03-23 20:02:08 +0300

    Добаить модельи пользователя и тодо

---
+++ Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Todo
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public string Text { get; set; }
+        public bool IsComplited { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
